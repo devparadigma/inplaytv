0d3efb7ff416670e27c44fe3bd78076a27d4178b
devops <77201587+devparadigma@users.noreply.github.com>
2023-10-20 17:15:31 +0300

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,64 +1,69 @@
 // ==UserScript==
-// @name         Inplayip.tv POST Request Extension
-// @namespace    https://example.com/
+// @name         Tampermonkey Request and Save JSON
+// @namespace    your-namespace
 // @version      1.0
-// @description  Auto POST request on inplayip.tv schedule page
-// @author       Your Name
-// @match        https://inplayip.tv/schedule
+// @description  Send a POST request and save the response as JSON file with today's date as filename
+// @match        https://inplayip.tv/shedule
 // @grant        GM_xmlhttpRequest
 // @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
-    // Получаем текущую дату и форматируем ее в необходимый формат (YYYY-MM-DD)
-    const today = new Date();
-    const searchDate = today.toISOString();
-    
-    // Создаем JSON объект с параметрами для запроса
-    const requestData = {
-        filters: {
-            searchDate: searchDate,
-            searchWord: "",
-            onlyNew: false,
-            showVOD: false,
-            showLive: false,
-            sportsCriteria: [],
-            countriesCriteria: [],
-            servicesCriteria: []
-        },
-        timezoneOffset: 0
-    };
-    
-    // Преобразуем объект в JSON строку
-    const jsonRequestBody = JSON.stringify(requestData);
-    
-    // Определяем обработчик для выполнения POST запроса
-    function sendPostRequest() {
+
+    // Функция для отправки POST запроса
+    function sendPostRequest(url, postData, callback) {
         GM_xmlhttpRequest({
             method: "POST",
-            url: "https://api.inplayip.tv/api/schedule/table",
+            url: url,
+            data: JSON.stringify(postData),
             headers: {
                 "Content-Type": "application/json"
             },
-            data: jsonRequestBody,
             onload: function(response) {
-                const jsonResult = JSON.parse(response.responseText); // Преобразуем полученный результат в JSON объект
-                
-                // Сохраняем JSON результат в файл
-                const fileData = JSON.stringify(jsonResult);
-                const fileName = "postResponse.json";
-                
-                // Выполняем загрузку файла
-                GM_download({
-                    url: "data:application/json;charset=utf-8," + encodeURIComponent(fileData),
-                    name: fileName
-                });
+                callback(response.responseText);
             }
         });
     }
-    
-    // Выполняем отправку POST запроса при загрузке страницы
-    window.addEventListener('load', sendPostRequest);
+
+    // Функция для сохранения JSON в файл
+    function saveJSONToFile(jsonData) {
+        const todayDate = new Date().toLocaleDateString().replace(/\//g, "-");
+        const fileName = todayDate + ".json";
+        const fileData = JSON.stringify(jsonData);
+        
+        const blobData = new Blob([fileData], { type: "application/json" });
+        const downloadLink = document.createElement("a");
+        downloadLink.href = window.URL.createObjectURL(blobData);
+        downloadLink.download = fileName;
+        downloadLink.click();
+    }
+
+    // Отправка POST запроса и сохранение JSON
+    function sendAndSaveRequest() {
+        const url = "https://api.inplayip.tv/api/schedule/table";
+        const searchDate = new Date().toISOString().split("T")[0]; // Получаем сегодняшнюю дату
+
+        const postData = {
+            filters: {
+                searchDate: searchDate,
+                searchWord: "",
+                onlyNew: false,
+                showVOD: false,
+                showLive: false,
+                sportsCriteria: [],
+                countriesCriteria: [],
+                servicesCriteria: []
+            },
+            timezoneOffset: 0
+        };
+
+        sendPostRequest(url, postData, function(responseData) {
+            const jsonData = JSON.parse(responseData);
+            saveJSONToFile(jsonData);
+        });
+    }
+
+    // Выполняем отправку запроса и сохранение при загрузке страницы
+    sendAndSaveRequest();
 })();
