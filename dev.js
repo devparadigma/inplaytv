726425e2e5ba28acc5aab6c7935d551a34528b7b
devops <77201587+devparadigma@users.noreply.github.com>
2023-10-20 17:09:53 +0300

    Update dev.js

--- dev.js
+++ dev.js
@@ -11,32 +11,13 @@
 
 (function() {
     'use strict';
-
-    function sendPostRequest(url, data) {
-        GM_xmlhttpRequest({
-            method: 'POST',
-            url: url,
-            data: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            onload: function(response) {
-                console.log(response.responseText);
-            },
-            onerror: function(error) {
-                console.error(error);
-            }
-        });
-    }
-
-    function getCurrentDate() {
-        var today = new Date();
-        var currentDate = today.toISOString();
-        return currentDate;
-    }
-
-    var searchDate = getCurrentDate();
-    var requestData = {
+    
+    // Получаем текущую дату и форматируем ее в необходимый формат (YYYY-MM-DD)
+    const today = new Date();
+    const searchDate = today.toISOString().split('T')[0];
+    
+    // Создаем JSON объект с параметрами для запроса
+    const requestData = {
         filters: {
             searchDate: searchDate,
             searchWord: "",
@@ -49,8 +30,35 @@
         },
         timezoneOffset: 0
     };
-
-    sendPostRequest("https://api.inplayip.tv/api/schedule/table", requestData);
-    sendPostRequest("https://jsonbase.devparadigma.workers.dev/devdata", requestData);
-
+    
+    // Преобразуем объект в JSON строку
+    const jsonRequestBody = JSON.stringify(requestData);
+    
+    // Определяем обработчик для выполнения POST запроса
+    function sendPostRequest() {
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://api.inplayip.tv/api/schedule/table",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: jsonRequestBody,
+            onload: function(response) {
+                const jsonResult = JSON.parse(response.responseText); // Преобразуем полученный результат в JSON объект
+                
+                // Сохраняем JSON результат в файл
+                const fileData = JSON.stringify(jsonResult);
+                const fileName = "postResponse.json";
+                
+                // Выполняем загрузку файла
+                GM_download({
+                    url: "data:application/json;charset=utf-8," + encodeURIComponent(fileData),
+                    name: fileName
+                });
+            }
+        });
+    }
+    
+    // Выполняем отправку POST запроса при загрузке страницы
+    window.addEventListener('load', sendPostRequest);
 })();
