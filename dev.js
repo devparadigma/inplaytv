b65d2c8ed22c5a42e95d82bc7c749e5cbcb4a6a9
devops <77201587+devparadigma@users.noreply.github.com>
2023-10-23 09:27:31 +0300

    Update dev.js

--- dev.js
+++ dev.js
@@ -3,7 +3,7 @@
 // @namespace    your-namespace
 // @version      1.0
 // @description  Send a POST request and save the response as JSON file with today's date as filename and send it to another API
-// @match        https://inplayip.tv/shedule
+// @match        https://inplayip.tv/*
 // @grant        GM_xmlhttpRequest
 // @run-at       document-end
 // ==/UserScript==
